983c33e8397f4723dd264581e7e105068e381607
Vaishali Kambli <vaishali.kambli1@outlook.com>
2019-09-16 23:08:01 -0400

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { AuthorsService } from './authors.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthorsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    AuthorsService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
